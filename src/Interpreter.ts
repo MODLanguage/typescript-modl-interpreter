c5650b2c70584006ab81ed838560f76d883595c5
Tony Walmsley <tony@aosd.co.uk>
2020-06-19 13:59:01 +0100

    Exclude generated JS files from Git.

--- src/Interpreter.ts
+++ src/Interpreter.ts
@@ -1,13 +1,35 @@
 import antlr4 = require("antlr4");
 import modlParser = require("./gen/MODL/MODLParser");
 import modlLexer = require("./gen/MODL/MODLLexer");
+import * as modl from './Model';
 
 export class Interpreter {
-    interpret(s: string) {
+    parse(s: string): modlParser.MODLParser {
         const inputStream = new antlr4.InputStream(s);
         const lexer = new modlLexer.MODLLexer(inputStream);
         const commonTokenStream = new antlr4.CommonTokenStream(lexer);
         const parser = new modlParser.MODLParser(commonTokenStream);
         return parser.modl();
+    }
+
+    interpretToJsonString(s: string): string {
+        this.parse(s);
+        let parser = this.interpret(s);
+        return "fail";
+    }
+
+    interpretToPrettyJsonString(s: string): string {
+        this.parse(s);
+        return "fail";
+    }
+
+    interpretToJsonObject(s: string): Object {
+        this.parse(s);
+        return {};
+    }
+
+    interpret(s: string): modl.Modl {
+        this.parse(s);
+        return new modl.Modl();
     }
 }
--- test/Interpreter.test.ts
+++ test/Interpreter.test.ts
@@ -1,16 +1,31 @@
 // @ts-ignore
+import {Modl} from "../src/Model";
+// let interpreter = require('../src/Interpreter.ts');
+import {Interpreter} from '../src/Interpreter';
+
 let mocha = require('mocha');
 // @ts-ignore
 let chai = require('chai');
 
-// let interpreter = require('../src/Interpreter.ts');
-import {Interpreter} from '../src/Interpreter';
-
 const expect = chai.expect;
 describe('TypeScript Interpreter', () => {
 
-    it('should be able to parse a simple MODL string', () => {
-        expect(new Interpreter().interpret('a=b'));
+    it('should be able to parse a simple MODL string to a JSON String', () => {
+        expect(new Interpreter().interpretToJsonString('a=b')).to.equal('{"a": "b"}');
+    });
+
+    it('should be able to parse a simple MODL string to a pretty JSON String', () => {
+        expect(new Interpreter().interpretToPrettyJsonString('a=b')).to.equal('{\n' +
+            '    "a": "b"\n' +
+            '}');
+    });
+
+    it('should be able to parse a simple MODL string to a JSON object', () => {
+        expect(new Interpreter().interpretToJsonObject('a=b')).to.equal(JSON.parse('{"a": "b"}'));
+    });
+
+    it('should be able to parse a simple MODL string to a Modl object', () => {
+        expect(new Interpreter().interpretToJsonObject('a=b')).to.eq(new Modl());
     });
 
 });
