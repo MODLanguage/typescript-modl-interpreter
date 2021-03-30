7f300046f24f53bb15588a9bb60182167a476c8a
Tony Walmsley <tony@aosd.co.uk>
2021-03-30 15:02:35 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const Interpreter = require('./dist/modl-interpreter/Interpreter.js');
+const Parser = require('./dist/modl-interpreter/Parser.js');
+
+module.exports = {
+  Interpreter: Interpreter,
+  Parser: Parser
+};
